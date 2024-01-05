e670394a863eddad57d1e06047dbbab44dfcbe85
weldengus <155380210+weldengus@users.noreply.github.com>
2024-01-05 17:58:50 -0500

    Update Ezra.java
    just cool

--- src/GitHup/Ezra.java
+++ src/GitHup/Ezra.java
@@ -2,6 +2,7 @@ package GitHup;
 
 public class Ezra {
 public static void main(String[] args) {
+	// I like the way you did it. just have fun and enjoy git and gitHup.
 	int a=10;
 	int b=26;
 	int sum=a+b;
